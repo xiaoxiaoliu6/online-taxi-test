19437ab875dbad05808d1af1bc34d788752f67bf
xiaoxiaoliu6 <1518359298@qq.com>
2022-12-06 17:46:35 +0800

    Add files via upload
    add logic of service -order-dispatch

---
+++ service-order-dispatch/src/main/java/com/online/taxi/order/dispatch/service/DispatchService.java
@@ -0,0 +1,17 @@
+package com.online.taxi.order.dispatch.service;
+
+import com.online.taxi.common.dto.ResponseResult;
+
+import java.util.List;
+
+
+public interface DispatchService {
+
+    /**
+     * 将指定订单拍给多个司机
+     * @param orderId
+     * @param driverIdList
+     * @return
+     */
+    public ResponseResult dispatch(int orderId , List<Integer> driverIdList);
+}
